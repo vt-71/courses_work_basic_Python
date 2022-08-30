673b323554f528ff52d9d45192f45746968073ee
vt-71 <tyanv01@gmail.com>
2022-08-30 15:59:16 +0300

    Осталось добавить вывод в .josn

--- course_work.py
+++ course_work.py
@@ -1,18 +1,17 @@
+# CourseWork Tyan V PY-59
+# Backup photo from VK to Yandex Disk/
+
 from unicodedata import name
 import requests
 from pprint import pprint
 import yadisk
 import time
-# import tkinter as tk
-# import tkinter.ttk as ttk
 from tqdm import tqdm 
     
-access_token = 'vk1.a.guBSUV6VmU9kPXdsPy9w0IOT590UWSG6kxuQCSsMIYrdURdd_v8PVKoiujjQjxuQH2QjDyRPqkaSB-tU7PmSb0PbbGE-NcfDMBrTTEr3FMfF4UvQXkl8uB3e5amd5RVXheCCIiDIqY6du16au1pUbDO4kidXUQH44yEdB8LYVku89XFQ8FJaieYC7lgYtgky'
-TOKEN_YA = 'y0_AQAAAAA_A4UUAADLWwAAAADLF6At6ut-A5tiQZ62UcSdetciEoASM_s'
+
 
 
 class VK:
-
    def __init__(self, access_token, user_id, version='5.131'):
        self.token = access_token
        self.id = user_id
@@ -20,13 +19,6 @@ class VK:
        self.params = {'access_token': self.token, 'v': self.version, 'album_id': 'profile', 'extended': 1}
 
 
-   def users_info(self):
-       url = 'https://api.vk.com/method/users.get'
-       params = {'user_ids': self.id}
-       response = requests.get(url, params={**self.params, **params})
-       return response.json()
-
- 
    def users_photo(self):
 # Метод парсит фото с ВК, переименовывает фото по лайкам.
         list_photo = []
@@ -37,7 +29,7 @@ class VK:
         for items in response.json()['response']['items']:
             items['name'] = (str(items['likes']['count']) + ".jpg")
             photo_info = items['sizes']       
-            for size in vk_size_type:  
+            for size in vk_size_type: # Делает список фото  максимального размера 
                 for photo in reversed(photo_info):
                     if size in photo['type']:
                         photo['name'] = items['name']
@@ -46,42 +38,53 @@ class VK:
                         break                    
                     break
         n=0
-        for max_photo in list_photo[n:]:
+        for max_photo in list_photo[n:]: #Сравнивает имена фото в итоговом списке, если одинаковые, добавляет дату.
             for i in list_photo[n+1:]:
                 if max_photo['name'] == i['name']:
                     max_photo['name'] = (str(max_photo['date']) + "_" + str(max_photo['name']))
                     list_photo[n] = max_photo
                     break
             n += 1 
-        # pprint(list_photo) 
         return list_photo
 
-   
+
+with open('tokens.txt', 'r',encoding='utf-8') as file_object: # Считывает токены из файла tokens.txt
+    list_file = file_object.readlines()
+    access_token =  list_file [1].strip()
+    TOKEN_YA =  list_file [3].strip()
 
 
+# if __name__ =="__main__":
 user_id = input('Введите id пользователя ВКонтакте: ')
 name_folder = input('Введите название папки: ')
-user_id = 1222
-# user_id = 7970141
+# user_id = 1222
 
 vk = VK(access_token, user_id)
-
 #######################
 list_photo = vk.users_photo()
+count_photo = 5
+print(f'Всего найдено {len(list_photo)} фотографий')
+print('Будет гружено 5 максимального размера')
+count_photo_user = input('Если хотите изменить количество, введите "да". Для продолжения нажмите Enter: ')
+
+if str.lower(count_photo_user) == 'да': 
+    count_photo_user = int(input(f'Введите количество загружаемых фото от 1 до {len(list_photo)}: ')) 
+    count_photo = count_photo_user
 
 y = yadisk.YaDisk(token=TOKEN_YA)
-y.mkdir("/" + name_folder) 
-
-
-for photo in tqdm(list_photo):
-    path = (name_folder + '/' + photo['name'])
-    y.upload_url(photo['url'], path)
-    time.sleep(0.33)   
-
-###############################
-
-# vk.users_photo()
-
-
-# pprint(vk.users_photo())
-# pprint(vk.users_info())
+try:       
+    y.mkdir("/" + name_folder) 
+    vk_size_type = ['w','z','y','r','q','p','o','x','m','s']
+    sort_size_photo = []
+    for size in vk_size_type:    
+        for photo in list_photo:
+            if size == photo['type']:
+              sort_size_photo.append(photo)
+            
+    for photo in tqdm(sort_size_photo[0:count_photo]):
+            path = (name_folder + '/' + photo['name'])
+            y.upload_url(photo['url'], path)
+            time.sleep(0.33)
+    print('Загрузка завершена')
+except:
+    print('Ошибка подключения или такая папка уже существует')   
